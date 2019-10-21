0bdee578636f6a7763e705d468f5a403cd268568
Bogdan7r <56310681+Bogdan7r@users.noreply.github.com>
2019-10-21 21:38:58 +0300

    Update ceasar.py

--- ceasar.py
+++ ceasar.py
@@ -1,5 +1,15 @@
 plaintext = input()
 def encrypt_ceasar(plaintext):
+    """
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
     ciphertext = ""
     for i in plaintext:
         if ((ord(i) > 64) and (ord(i) < 91)) or (ord(i) > 96) and (ord(i) < 123):
@@ -21,6 +31,16 @@ encrypt_ceasar(plaintext)
 
 ciphertext = input()
 def encrypt_ceasar(ciphertext):
+    """
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
     plaintext = ""
     for i in ciphertext:
         if ((ord(i) > 64) and (ord(i) < 91)) or (ord(i) > 96) and (ord(i) < 123):
